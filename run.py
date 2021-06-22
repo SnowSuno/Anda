a06cd27b41e596dacdf562a32e6d1aa73487d389
SnowSuno <percy3368@gmail.com>
2021-06-23 02:43:06 +0900

    feat : add debug mode

--- run.py
+++ run.py
@@ -1,9 +1,24 @@
+import os
+import sys
+
+from data.settings import USER_ID, USER_PW, DEVICE_NAME, DEVICE_UUID
 from src.main import AndaClient
-from data.private import KAKAO_ID, KAKAO_PW, DEVICE_NAME, DEVICE_UUID
+from src.system.console import Console
 from src.system.exception_handler import exception_handler
+
+DEBUG = True
+
+def run():
+    bot = AndaClient(DEVICE_NAME, DEVICE_UUID)
+
+    if not DEBUG:
+        bot.loop.set_exception_handler(exception_handler)
+
+    bot.run(USER_ID, USER_PW)
 
 
 if __name__ == '__main__':
-    bot = AndaClient(DEVICE_NAME, DEVICE_UUID)
-    bot.loop.set_exception_handler(exception_handler)
-    bot.run(KAKAO_ID, KAKAO_PW)
+    print('')
+    Console.log('Server start')
+    run()
+    os.execv(sys.executable, ['python'] + sys.argv)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from KakaoBot.kakaopy.client import Client
 
 class AndaClient(Client):
-    pass
-
+    async def on_message(self, chat):
+        if chat.message.startswith('안다'):
+            await chat.send_text('안다가 곧 부활합니당!!')
