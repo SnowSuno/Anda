abe2441ae8ef7c39b827c8f554b9dac475d0f444
SnowSuno <percy3368@gmail.com>
2021-06-23 02:53:32 +0900

    fix : change setting method of debug/deploy mode as sys.argv

--- run.py
+++ run.py
@@ -6,19 +6,23 @@ from src.main import AndaClient
 from src.system.console import Console
 from src.system.exception_handler import exception_handler
 
-DEBUG = True
 
-def run():
+def run(run_mode):
     bot = AndaClient(DEVICE_NAME, DEVICE_UUID)
 
-    if not DEBUG:
+    if run_mode == 'deploy':
         bot.loop.set_exception_handler(exception_handler)
+    elif run_mode == 'debug':
+        pass
+    else:
+        raise Exception('Not a valid mode')
 
     bot.run(USER_ID, USER_PW)
 
 
 if __name__ == '__main__':
+    mode = sys.argv[1] if len(sys.argv) == 2 else 'debug'
     print('')
     Console.log('Server start')
-    run()
+    run(mode)
     os.execv(sys.executable, ['python'] + sys.argv)
